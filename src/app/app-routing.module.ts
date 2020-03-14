c47e817b6f02f678fd774ce8b7d1f33f82a9e13a
utkarsh_lohani <utkarsh.lohani@mavenwave.com>
2020-03-15 00:22:50 +0530

    Fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NavbarComponent } from './Components/navbar/navbar.component';
 import { UserloginComponent } from './Components/userlogin/userlogin.component';
 import { QuizcreateComponent } from './Components/quizcreate/quizcreate.component';
 import { QuizdisplayComponent } from './Components/quizdisplay/quizdisplay.component';
@@ -10,7 +9,6 @@ import { HomeComponent } from './Components/home/home.component';
 const routes: Routes = [
   { path: '', component: UserloginComponent },
   { path: 'userlogin', component: UserloginComponent },
-  // { path: 'navbar', component: NavbarComponent },
   { path: 'quizcreate', component: QuizcreateComponent },
   { path: 'quizdisplay', component: QuizdisplayComponent },
   { path: 'registration', component: RegistrationComponent },
