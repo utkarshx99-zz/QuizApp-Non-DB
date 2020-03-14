417701cc6ae5de7595377b3e57b07fe086b8e0d7
utkarsh_lohani <utkarsh.lohani@mavenwave.com>
2020-03-14 22:31:15 +0530

    Functionality Added (Beta)

--- src/app/Components/quizdisplay/quizdisplay.component.ts
+++ src/app/Components/quizdisplay/quizdisplay.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/Services/login.service';
+import { QuizDataService } from 'src/app/Services/quiz-data.service';
 
 @Component({
   selector: 'app-quizdisplay',
@@ -9,11 +10,13 @@ import { LoginService } from 'src/app/Services/login.service';
 export class QuizdisplayComponent implements OnInit {
 
   public LoginContent = [];
+  public QuizDataContent = [];
 
-  constructor(private ls: LoginService) { }
+  constructor(private ls: LoginService, private qs: QuizDataService) { }
 
   ngOnInit(): void {
     this.LoginContent = this.ls.fetchLoginData();
+    this.QuizDataContent = this.qs.fetchQuizData();
   }
 
 }
