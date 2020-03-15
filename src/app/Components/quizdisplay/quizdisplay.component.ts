619611669b5c23ff2643f5ec3f7faf658c4a51fe
utkarsh_lohani <utkarsh.lohani@mavenwave.com>
2020-03-15 19:51:03 +0530

    Extra Question Functionality Enhanced

--- src/app/Components/quizdisplay/quizdisplay.component.ts
+++ src/app/Components/quizdisplay/quizdisplay.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/Services/login.service';
 import { QuizDataService } from 'src/app/Services/quiz-data.service';
+import { ExtraQuestionService } from 'src/app/Services/extra-question.service';
 
 @Component({
   selector: 'app-quizdisplay',
@@ -11,12 +12,14 @@ export class QuizdisplayComponent implements OnInit {
 
   public LoginContent = [];
   public QuizDataContent = [];
+  public ExtraQuizQuestion = [];
 
-  constructor(private ls: LoginService, private qs: QuizDataService) { }
+  constructor(private ls: LoginService, private qs: QuizDataService, private eq: ExtraQuestionService) { }
 
   ngOnInit(): void {
     this.LoginContent = this.ls.fetchLoginData();
     this.QuizDataContent = this.qs.fetchQuizData();
+    this.ExtraQuizQuestion = this.eq.fetchExtraQuestionData();
   }
 
 }
